00ca1e155f13b10583dc45433108670f47677268
Marcus Gartner <marcus@fanium.com>
2013-07-12 17:20:42 -0400

    clean up

--- app/leaky_view.rb
+++ app/leaky_view.rb
@@ -1,7 +1,8 @@
 class LeakyView < UIView
 
   def leak
-    UIView.animateWithDuration(1.5, animations: lambda { puts "yo" })  end
+    UIView.animateWithDuration(1.5, animations: lambda { puts "yo" })
+  end
 
   def dealloc
     puts "LeakyView deallocated"
